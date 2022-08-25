002e77fd1305244d383986a717cae183ad21b614
GrandMoff100 <nlarsen23.student@gmail.com>
2022-08-24 19:24:35 -0500

    Add example testing file

---
+++ example.py
@@ -0,0 +1,19 @@
+# from typing import Any
+#
+#
+# class A:
+#     a_attr: int = "1"
+#
+#
+# class MyClass(A):
+#     def __init__(obj, b: dict[str, Any]):
+#         obj.b = b
+#
+#     def mymethod(self):
+#         return self.c + 5 + self.a_attr - self.b
+#
+#
+# def func(a: MyClass, b: "MyClass"):  # type: (MyClass, MyClass) -> int
+#     return a.mymethod()
+
+1 + "2" + {}
